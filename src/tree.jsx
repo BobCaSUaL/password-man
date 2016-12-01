791e8b27a0270ee86d28dcdf16202e408eef0396
Roberto CASULA <roberto.casula@edu.unito.it>
2016-12-01 22:46:22 +0100

    added back button and rendere for entry

--- src/tree.jsx
+++ src/tree.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import component from 'functional-react'
-import {Glyphicon, Media, Grid} from 'react-bootstrap'
+import {Button, Glyphicon, Grid, Media, Navbar} from 'react-bootstrap'
 import {dispatch} from './reducer'
 
 const inspect = (obj) => require('util').inspect(obj, { depth: null })
@@ -8,6 +8,10 @@ const inspect = (obj) => require('util').inspect(obj, { depth: null })
 const select = path => () => dispatch({
    type: '/tree/SELECT',
    data: { selectedPath: path }
+})
+const backPath = path => () => dispatch({
+   type: '/tree/SELECT',
+   data: { selectedPath: path.slice(0, -1) }
 })
 
 
@@ -19,39 +23,82 @@ const selected = ({...tree, selectedPath}) =>
       }) : tree
 
 const renderCategory = ({tree, path}, handle) => (
-   <Grid>
-      <Media.List>
-         {tree.children.map(({...child, children}, index) => (
-            <Media.ListItem key={index}
-               onClickCapture={handle(select(path.concat(index)))}>
-               <Media.Left>
-                  <Glyphicon glyph={child.data.icon} />
-               </Media.Left>
-               <Media.Body>
-                  <Media.Heading>{child.data.name}</Media.Heading>
-                  {children && children.length > 0 && _render({
-                     tree: {
-                        type: 'recursion',
-                        children
-                     },
+   <Media.List>
+      {tree.children.map(({...child, children}, index) => (
+         <Media.ListItem key={index}
+            onClickCapture={handle(select(path.concat(index)))}>
+            <Media.Left>
+               <Glyphicon glyph={child.data.icon} />
+            </Media.Left>
+            <Media.Body>
+               <Media.Heading>{child.data.name}</Media.Heading>
+               {children && children.length > 0 && _render({
+                  tree: {
+                     type: child.type,
+                     children
+                  },
                      path: path.concat(index)
                   }, handle)
-                  }
-               </Media.Body>
-               <Media.Right>
-                  <Glyphicon glyph="chevron-right" />
-               </Media.Right>
-            </Media.ListItem>
-         ))}
+               }
+            </Media.Body>
+            <Media.Right>
+               <Glyphicon glyph="chevron-right" />
+            </Media.Right>
+         </Media.ListItem>
+      ))}
 
-         {path.length <= 0 && (<pre>{inspect(tree)}</pre>)}
-      </Media.List>
-   </Grid>
+      {path.length <= 0 && (<pre>{inspect(tree)}</pre>)}
+   </Media.List>
+)
+const renderEntry = ({tree, path}, handle) => (
+   <Media key={path.slice(-1)}>
+      <Media.Left>
+         <Glyphicon glyph={tree.data.icon} />
+      </Media.Left>
+      <Media.Body>
+         <Media.Heading>{tree.data.name}</Media.Heading>
+      </Media.Body>
+      <Media.Right>
+         <Glyphicon glyph="chevron-right" />
+      </Media.Right>
+   </Media>
 )
 
-const _render = ({tree, path}, handle) => renderCategory({tree, path}, handle)
-const render = ({props: {tree}}, handle) => _render({
-   tree: selected(tree), path: tree.selectedPath
-}, handle)
+const _render = ({tree, path}, handle) => do {
+   tree.type === 'root' ?
+      renderCategory({tree, path}, handle) :
+   tree.type === 'category' ?
+      renderCategory({tree, path}, handle) :
+   tree.type === 'entry' ?
+      renderEntry({tree, path}, handle) :
+   null
+}
+const render = ({props: {tree}}, handle) => (
+   <div>
+      <Navbar>
+         <Navbar.Header>
+            <Button bsStyle="primary" bsSize="large"
+               onClickCapture={handle(backPath(tree.selectedPath))}>
+               <Glyphicon glyph={
+                  tree.selectedPath.length > 0 ?
+                     'chevron-left' : 'menu-hamburger'
+                  }/>
+            </Button>
+         </Navbar.Header>
+         <Navbar.Header>
+            <Navbar.Brand>
+               PasswordMan
+            </Navbar.Brand>
+         </Navbar.Header>
+      </Navbar>
+      <Grid>
+         {
+            _render({
+               tree: selected(tree), path: tree.selectedPath
+            }, handle)
+         }
+      </Grid>
+   </div>
+)
 
 export default component('Tree', {render})
